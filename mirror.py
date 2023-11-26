f141bbea6f88ff61209b02ebbdcf4ffa205c2ec7
Seb <54874137+tarvoscsuf@users.noreply.github.com>
2023-11-25 22:18:42 -0800

    Updated to work with OpenAI 2023
    Also put Irvine USA as the standard city and RSS URL is included

--- mirror.py
+++ mirror.py
@@ -2,7 +2,8 @@ import tkinter as tk
 import time
 import requests
 import json
-import openai
+from openai import OpenAI
+import os
 
 # Backdrop, main window
 window = tk.Tk()
@@ -10,7 +11,10 @@ window.configure(bg='black')  # Set the window background color to black
 window.attributes('-fullscreen', True)  # Set the window to fullscreen
 
 # Time label
-time_label = tk.Label(window, font=('Tahoma Bold', 100, 'bold'), fg='white', bg='black')
+time_label = tk.Label(window,
+                      font=('Tahoma Bold', 100, 'bold'),
+                      fg='white',
+                      bg='black')
 time_label.pack(expand=True)
 
 # Weather label
@@ -26,64 +30,63 @@ news_label = tk.Label(window, font=('Tahoma', 36), fg='white', bg='black')
 news_label.pack(side=tk.BOTTOM, pady=40)
 
 # New York Times RSS API URL
-rss_url = "KEY"
+rss_url = "https://rss.nytimes.com/services/xml/rss/nyt/US.xml"
 
 # OpenWeatherMap API
-weather_key = "KEY"
+weather_key = os.environ['OPENWEATHER_KEY']
 
 # OpenAI Token
-openai.api_key = "KEY"
+client = OpenAI(api_key = os.environ['OPENAI_KEY'])
 
 # Time updater
 def update_time():
-    # Get the current time
-    current_time = time.strftime('%H:%M:%S')
-    time_label.config(text=current_time)
+  # Get the current time
+  current_time = time.strftime('%H:%M:%S')
+  time_label.config(text=current_time)
 
-    # Schedule the next time update after 1 second (1000 milliseconds)
-    window.after(1000, update_time)
+  # Schedule the next time update after 1 second (1000 milliseconds)
+  window.after(1000, update_time)
+
 
 # Weather updater
 def update_weather():
-    try:
-        # Get the local weather from OpenWeatherMap API
-        # Enter your city name and country code below
-        city = "CITY"
-        country_code = "US"
+  try:
+    # Get the local weather from OpenWeatherMap API
+    # Enter your city name and country code below
+    city = "IRVINE"
+    country_code = "US"
 
-        # Send a request to OpenWeatherMap API with units=imperial for Fahrenheit
-        url = f"http://api.openweathermap.org/data/2.5/weather?q={city},{country_code}&appid={weather_key}&units=imperial"
-        response = requests.get(url)
-        data = json.loads(response.text)
+    # Send a request to OpenWeatherMap API with units=imperial for Fahrenheit
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city},{country_code}&appid={weather_key}&units=imperial"
+    response = requests.get(url)
+    data = json.loads(response.text)
 
-        # Extract relevant weather information
-        temperature = data["main"]["temp"]
-        weather_description = data["weather"][0]["description"]
+    # Extract relevant weather information
+    temperature = data["main"]["temp"]
+    weather_description = data["weather"][0]["description"]
 
-        # Update the weather label
-        weather_text = f"feels like {temperature}°F\nlooks like {weather_description}"
-        weather_label.config(text=weather_text)
-    except Exception as e:
-        print("Error:", str(e))
-        weather_label.config(text="Failed to fetch weather data.")
+    # Update the weather label
+    weather_text = f"feels like {temperature}°F\nlooks like {weather_description}"
+    weather_label.config(text=weather_text)
+  except Exception as e:
+    print("Error:", str(e))
+    weather_label.config(text="Failed to fetch weather data.")
 
-    # Schedule the next weather update after 30 minutes (1800000 milliseconds)
-    window.after(1800000, update_weather)
+  # Schedule the next weather update after 30 minutes (1800000 milliseconds)
+  window.after(1800000, update_weather)
 
 # Greetings updater
 def update_greeting():
     try:
         # Get the current time
         current_time = time.strftime('%H:%M:%S')
-	# Generate OpenAI response
-        openai_response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=f"tell me a greeting, it is currently {current_time} o'clock, be a bit lighthearted and playful",
-            max_tokens=140,
-            temperature=0.5,
-        )
-	# Format and display greeting
-        greeting_text = openai_response.choices[0].text.strip()
+  # Generate OpenAI response
+        completion = client.completions.create(
+          model="gpt-3.5-turbo-instruct",
+          prompt="Write a tagline for an ice cream shop."
+          )
+  # Format and display greeting
+        greeting_text = completion.choices[0].text.strip()
         greetings_label.config(text=greeting_text)
     except Exception as e:
         print("Error:", str(e))
@@ -92,35 +95,37 @@ def update_greeting():
     # Schedule the next greetings update after 30 seconds (30000 milliseconds)
     window.after(30000, update_greeting)
 
+
 # RSS updater
 def update_news():
-    try:
-        # Fetch the news headlines from the New York Times RSS API
-        response = requests.get(rss_url)
-        data = response.text
+  try:
+    # Fetch the news headlines from the New York Times RSS API
+    response = requests.get(rss_url)
+    data = response.text
 
-        # Parse the XML data
-        from xml.etree import ElementTree as ET
-        root = ET.fromstring(data)
+    # Parse the XML data
+    from xml.etree import ElementTree as ET
+    root = ET.fromstring(data)
 
-        # Extract the headlines from the XML
-        headlines = [item.find("title").text for item in root.iter("item")]
+    # Extract the headlines from the XML
+    headlines = [item.find("title").text for item in root.iter("item")]
 
-        # Update the news label with the next headline
-        if hasattr(update_news, 'headline_index'):
-            update_news.headline_index += 1
-        else:
-            update_news.headline_index = 0
-        if update_news.headline_index >= len(headlines):
-            update_news.headline_index = 0
+    # Update the news label with the next headline
+    if hasattr(update_news, 'headline_index'):
+      update_news.headline_index += 1
+    else:
+      update_news.headline_index = 0
+    if update_news.headline_index >= len(headlines):
+      update_news.headline_index = 0
 
-        news_label.config(text=headlines[update_news.headline_index])
-    except Exception as e:
-        print("Error:", str(e))
-        news_label.config(text="Failed to fetch news headlines.")
+    news_label.config(text=headlines[update_news.headline_index])
+  except Exception as e:
+    print("Error:", str(e))
+    news_label.config(text="Failed to fetch news headlines.")
 
-    # Schedule the next news update after 30 seconds (30000 milliseconds)
-    window.after(30000, update_news)
+  # Schedule the next news update after 30 seconds (30000 milliseconds)
+  window.after(30000, update_news)
+
 
 # Function calls
 update_time()
